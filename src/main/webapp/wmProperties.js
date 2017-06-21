8fcaf91c18c670fc9ae3d080b53b491c8bafd9e0
root <root@c0eae171a3f3>
2017-06-21 00:43:44 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_HCZbLtkWeJ",
+  "homePage" : "Main",
+  "name" : "Auto_HCZbLtkWeJ",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
